fb29a45bbfe51ff7785700f5288d5f6f8bf301e7
Ignacio P <51886210+Pinto2291@users.noreply.github.com>
2023-03-12 22:28:20 -0400

    added new html and css, more sections and JS effect

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let sidebar = document.getElementById('sidebar');
 let mainBody = document.querySelector('body');
 
 let active = false;
-
+/*
 buttonNav.addEventListener('click', () => {
 
     if(active){
@@ -16,13 +16,23 @@ buttonNav.addEventListener('click', () => {
         active = true;
     }
 });
+*/
 
-function hideSidebar() {
-    if(sidebar.style.width == 'var(--sidebar-width)' && mainBody.style.marginLeft == 'var(--sidebar-width)'){
-        mainBody.style.marginLeft = '0';
-        sidebar.style.width = '0';
+window.addEventListener('scroll', () => {
+    let revealElements = document.querySelectorAll('.reveal');
+
+    for(let i = 0; i < revealElements.length; i++ ){
+
+        let windowHeight = window.innerHeight;
+        let revealTop = revealElements[i].getBoundingClientRect().top;
+        let revealPoint = 120;
+
+        if(revealTop < windowHeight - revealPoint){
+            revealElements[i].classList.add('active');
+        } else {
+            revealElements[i].classList.remove('active');
+        }
     }
-}
+})
 
-hideSidebar();
-console.log(sidebar.style.display == 'none');
+
